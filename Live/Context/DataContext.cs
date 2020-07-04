ac2ec2c69426e371a0dcd6c0ddc7b023a01248e3
Daniel Rosa da Silveira <danielrosasilveira@gmail.com>
2020-07-04 12:06:05 -0300

    project

---
+++ Live/Context/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Live.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Live.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Client> Clients { get; set; }
+
+
+    }
+}
---
+++ Live/Models/Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Live.Models
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        [MinLength(3, ErrorMessage ="Mínimo 3 caracteres")]
+        [Display(Name ="Nome:")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage ="Obrigatório")]
+        [EmailAddress(ErrorMessage ="E-mail Inválido")]
+        [Display(Name ="E-mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        [Display(Name ="Data de Nascimento")]
+        public DateTime DtNasc { get; set; }
+
+        [Display(Name ="Ativo")]
+        public bool Status { get; set; }
+
+    }
+}
